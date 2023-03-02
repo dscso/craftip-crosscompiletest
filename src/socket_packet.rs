b2b672c6c888ca953fd8e29130f2a796daa33582
Jurek <git@jurek.io>
2023-03-02 15:44:52 +0100

    little macro magic

--- src/socket_packet.rs
+++ src/socket_packet.rs
@@ -6,7 +6,6 @@ use crate::minecraft::{MinecraftDataPacket, MinecraftHelloPacket, MinecraftPacke
 use crate::packet_codec::{PacketCodecError};
 use crate::proxy::{ProxyHelloPacket, ProxyPacket};
 
-
 #[derive(Debug)]
 pub enum SocketPacket {
     MinecraftPacket(MinecraftPacket),
@@ -14,38 +13,36 @@ pub enum SocketPacket {
     UnknownPacket,
 }
 
-impl SocketPacket {
-    pub fn new_first_package(packet: Vec<u8>) -> (Result<Option<SocketPacket>, PacketCodecError>, Protocol) {
-        // check if it is MC packet
-        tracing::info!("checking if its a mc pkg");
-        let hello_packet = MinecraftHelloPacket::new(packet.clone());
+macro_rules! packet_match {
+    ($socket_type:ident, $packet_type:ident, $variant:ident, $protocol_type:ident, $buffer:expr) => {
+        {
+        let hello_packet = $packet_type::new($buffer.clone());
         match hello_packet {
             Ok(hello_packet) => {
-                let protocol = Protocol::MC(hello_packet.version as u32);
-                return (Ok(Some(SocketPacket::MinecraftPacket(MinecraftPacket::MCHelloPacket(hello_packet)))), protocol);
+                let protocol = Protocol::$protocol_type(hello_packet.version as u32);
+                return (Ok(Some(SocketPacket::$socket_type($socket_type::$variant(hello_packet)))), protocol);
             }
             Err(PacketError::TooSmall) => {}
             Err(PacketError::NotMatching) => {}
             Err(e) => return (Err(PacketCodecError::PacketCodecError(e)), Protocol::Unknown),
         }
-        tracing::info!("its not a mc pkg");
-        // check if it is proxy packet
-        let hello_packet = ProxyHelloPacket::new(packet);
-        match hello_packet {
-            Ok(hello_packet) => {
-                let protocol = Protocol::Proxy(hello_packet.version as u32);
-                return (Ok(Some(SocketPacket::ProxyPacket(ProxyPacket::HelloPacket(hello_packet)))), protocol);
-            }
-            Err(PacketError::TooSmall) => {}
-            Err(e) => return (Err(PacketCodecError::PacketCodecError(e)), Protocol::Unknown),
         }
+    };
+}
+impl SocketPacket {
+    pub fn new_first_package(packet: Vec<u8>) -> (Result<Option<SocketPacket>, PacketCodecError>, Protocol) {
+        // check if it is MC packet
+        tracing::info!("checking if its a mc pkg");
+        packet_match!(MinecraftPacket, MinecraftHelloPacket, MCHelloPacket, MC, packet);
+        tracing::info!("its not a mc pkg");
+        packet_match!(ProxyPacket, ProxyHelloPacket, HelloPacket, Proxy, packet);
         (Ok(None), Protocol::Unknown)
     }
     /// gigantic match statement to determine the packet type
-    pub fn new(packet: Vec<u8>, protocol: Protocol) -> Result<Option<SocketPacket>, PacketCodecError> {
+    pub fn new(buf: Vec<u8>, protocol: Protocol) -> Result<Option<SocketPacket>, PacketCodecError> {
         match protocol {
             Protocol::MC(_) => {
-                let packet = MinecraftDataPacket::new(packet);
+                let packet = MinecraftDataPacket::new(buf);
                 match packet {
                     Ok(packet) => {
                         return Ok(Some(SocketPacket::MinecraftPacket(MinecraftPacket::MCDataPacket(packet))));
@@ -55,7 +52,7 @@ impl SocketPacket {
                 }
             }
             Protocol::Proxy(_) => {
-                let packet = ProxyPacket::new(packet);
+                let packet = ProxyPacket::new(buf);
                 match packet {
                     Ok(packet) => {
                         return Ok(Some(SocketPacket::ProxyPacket(packet)));
